a048e8852aaf47238b9c24ece6134e5f86d3f982
Catriona Ferguson <114424301+cferg97@users.noreply.github.com>
2023-01-13 09:46:40 +0000

    test

--- src/api/media/index.js
+++ src/api/media/index.js
@@ -1,10 +1,25 @@
 import express from "express";
 import uniqid from "uniqid";
 import httpErrors from "http-errors";
+import * as dotenv from "dotenv";
 import { getMedia, writeMedia } from "../../lib/fs-tools.js";
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { v2 as cloudinary } from "cloudinary";
+
+dotenv.config();
 
 const mediaRouter = express.Router();
 
+const cloudinaryPosterUpload = multer({
+    storage: new CloudinaryStorage({
+      cloudinary,
+      params: {
+        folder: "media/posters",
+      },
+    }),
+  }).single("poster");
+  
 mediaRouter.get("/", async (req, res, next) => {
   try {
     const media = await getMedia();
@@ -13,5 +28,52 @@ mediaRouter.get("/", async (req, res, next) => {
     next(err);
   }
 });
+
+mediaRouter.get("/:id", async (req, res, next) => {
+  try {
+    const media = await getMedia();
+    const match = media.find((m) => m.imdbID === req.params.id);
+    res.send(match);
+  } catch (err) {
+    next(err);
+  }
+});
+
+mediaRouter.post("/", async (req, res, next) => {
+  try {
+    const newMedia = {
+      ...req.body,
+      imdbID: uniqid(),
+    };
+    const media = await getMedia();
+    media.push(newMedia);
+    await writeMedia(media);
+    res.status(201).send({ id: newMedia.imdbID });
+  } catch (err) {
+    next(err);
+  }
+});
+
+mediaRouter.post(
+  "/:id/poster",
+  cloudinaryPosterUpload,
+  async (req, res, next) => {
+    try {
+      const media = await getMedia();
+      const index = media.findIndex((m) => m.imdbID === req.params.id);
+      if (index !== -1) {
+        const oldMedia = media[index];
+        const updated = { ...oldMedia, poster: req.file.path };
+        media[index] = updated;
+        await writeMedia(media);
+        res.send(updated);
+      } else {
+        res.status(404).send(`Could not find media with ID ${req.params.id}`);
+      }
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 export default mediaRouter;
